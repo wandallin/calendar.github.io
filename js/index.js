1cf4719fe3ff319ab88bae36cd45df676eabef8c
wandallin <wandal_lin@hotmail.com>
2017-05-25 17:03:46 +0800

    calendar title fixed

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    weeks: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+    times: ['1a','2a','3a','4a','5a','6a',
+            '7a','8a','9a','10a','11a','12',
+            '1p','2p','3p','4p','5p','6p',
+            '7p','8p','9p','10p','11p']
+  }
+});
